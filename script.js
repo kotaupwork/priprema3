aad2b7c00621a82b00313971330fdb6a09a35d29
kota011 <59875539+kota011@users.noreply.github.com>
2023-03-21 12:50:40 +0100

    novve ikonice

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 //https://taylorvowell.com/
 //https://www.rubens.design/
 //https://jopecuro.com/#Home
+https://baloo.rs/?gclid=CjwKCAjwq-WgBhBMEiwAzKSH6MvQvScmsaNBaFG7ObfNBDEH7DzNoBVV4Rff_rIqKxfwbpleF9OF7xoCoIsQAvD_BwE
