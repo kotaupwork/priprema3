872b1b6252f7d8fb6906d56f8c9ab26996c3751d
kota011 <59875539+kota011@users.noreply.github.com>
2023-03-23 11:58:45 +0100

    dodat script2

--- script.js
+++ script.js
@@ -26,11 +26,3 @@ document.querySelector("h2").onmouseover = event => {
     iteration += 1 / 3;
   }, 30);
 }
-
-
-//https://www.htmlcssbuttongenerator.com/css-button-generator.html
-//https://codepen.io/Hyperplexed/pen/rNrJgrd
-//https://taylorvowell.com/
-//https://www.rubens.design/
-//https://jopecuro.com/#Home
-//https://baloo.rs/?gclid=CjwKCAjwq-WgBhBMEiwAzKSH6MvQvScmsaNBaFG7ObfNBDEH7DzNoBVV4Rff_rIqKxfwbpleF9OF7xoCoIsQAvD_BwE
