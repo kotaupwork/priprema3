6992067169a8e87ca96ba90f0a8835601a236dfd
kota011 <59875539+kota011@users.noreply.github.com>
2023-03-23 23:27:57 +0100

    promenjen raspored

--- script2.js
+++ script2.js
@@ -9,25 +9,25 @@ for (var i = 0; i <4; i++)
   var word = document.createElement("span");
   
   word.innerHTML = `${text} `;
-  
   word.classList.add("card-subtitle-word");
-  
   word.style.transitionDelay = `${index * 40}ms`;
-  
   return word;
 }
+
 var addWord = (text, index) => subtitle.appendChild(createWord(text, index));
 
 var createSubtitle = text => text.split(" ").map(addWord);
 switch (i) {
   case 0:
     createSubtitle("Website design");
+    subtitle.appendChild(document.createElement("br"));
+    createSubtitle("New line");
     break;
   case 1:
     createSubtitle("Menu, Banner and Visit card design");
     break;
   case 2:
-    createSubtitle("Social medi smh smh");
+    createSubtitle("Social media smh smh");
     break;
   default:
     createSubtitle("NFC tag setup, QR code print");
